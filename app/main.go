3421db25ebf9eb76cf962fe98fa5e9f6e67e77c1
zxy <3274315379@qq.com>
2025-08-02 15:16:04 +0800

    feature:send a PONG response

--- app/main.go
+++ app/main.go
@@ -21,9 +21,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	_, err = conn.Write([]byte("+PONG\\r\\n"))
+	if err != nil {
+		fmt.Println("Failed to write")
+		return
 	}
 }
