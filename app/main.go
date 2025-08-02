f1d99e8af3f4e1a7a4daaca1009d3808706b1fbc
zxy <3274315379@qq.com>
2025-08-02 15:48:33 +0800

    feature:Handle concurrent clients

--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
@@ -22,21 +21,25 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 	for {
-		buf := make([]byte, 1024)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	buf := make([]byte, 1024)
+	for {
 		_, err := conn.Read(buf)
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
 			fmt.Println("Failed to read")
 			return
 		}
+		fmt.Println("Received: ", string(buf))
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			fmt.Println("Failed to write")
